89660a5316d4c55dc2b6530e8419f7c03b1e2468
Rafiere <rafapgbr@gmail.com>
2024-01-26 15:10:38 -0300

    feat: add event dispatcher register method test

---
+++ utils/pkg/events/event_dispatcher_test.go
@@ -0,0 +1,88 @@
+package events
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+	ID int
+}
+
+/* O código não será repetido por causa da suíte. */
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}
+
+/* Esse método será executado a cada teste para preparar os dados que serão utilizados. */
+func (suite *EventDispatcherTestSuite) SetupTest() {
+	suite.eventDispatcher = NewEventDispatcher()
+	suite.handler = TestEventHandler{
+		ID: 1,
+	}
+	suite.handler2 = TestEventHandler{
+		ID: 2,
+	}
+	suite.handler3 = TestEventHandler{
+		ID: 3,
+	}
+	suite.event = TestEvent{
+		Name:    "test",
+		Payload: "test",
+	}
+	suite.event2 = TestEvent{
+		Name:    "test2",
+		Payload: "test2",
+	}
+}
+
+func (h *TestEventHandler) Handle(event EventInterface) {
+
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+
+	//Estamos registrando um evento e um handler.
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+
+	suite.Nil(err)
+
+	suite.Equal(1, len(suite.eventDispatcher.handlers))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+
+	suite.Equal(2, len(suite.eventDispatcher.handlers))
+
+	assert.Equal(suite.T(), &suite.handler, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][1])
+}
