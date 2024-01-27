98d7f4896a6add8eb56bdc4a5a969fe4bf84041f
Rafiere <rafapgbr@gmail.com>
2024-01-27 09:58:28 -0300

    feat: add update balance method

--- microsservico_wallet-core/internal/gateway/account.go
+++ microsservico_wallet-core/internal/gateway/account.go
@@ -5,4 +5,5 @@ import "github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsserv
 type AccountGateway interface {
 	Save(account *entity.Account) error
 	FindByID(id string) (*entity.Account, error)
+	UpdateBalance(account *entity.Account) error
 }
