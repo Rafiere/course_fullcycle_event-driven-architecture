059d8ddebd0c7dc62cadea13b2795d19118d69ac
Rafiere <rafapgbr@gmail.com>
2024-01-27 09:57:03 -0300

    feat: add mocks

---
+++ microsservico_wallet-core/internal/usecase/mocks/transaction_gateway.go
@@ -0,0 +1,15 @@
+package mocks
+
+import (
+	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/entity"
+	"github.com/stretchr/testify/mock"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
+	args := m.Called(transaction)
+	return args.Error(0)
+}
