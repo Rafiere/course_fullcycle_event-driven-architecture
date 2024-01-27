940210670de9128a2bb62d187ee21e8364dbf777
Rafiere <rafapgbr@gmail.com>
2024-01-26 22:28:29 -0300

    refactor! change method name

--- microsservico_wallet-core/internal/database/account_db.go
+++ microsservico_wallet-core/internal/database/account_db.go
@@ -15,7 +15,7 @@ func NewAccountDB(db *sql.DB) *AccountDB {
 	}
 }
 
-func (a *AccountDB) Get(id string) (*entity.Account, error) {
+func (a *AccountDB) FindByID(id string) (*entity.Account, error) {
 	var account entity.Account
 	var client entity.Client
 
