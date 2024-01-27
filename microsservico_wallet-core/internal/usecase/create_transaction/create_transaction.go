f739ceb8faffb19aad329d7f42ba6da2206ac021
Rafiere <rafapgbr@gmail.com>
2024-01-27 14:16:38 -0300

    feat: add create transaction and tests

--- microsservico_wallet-core/internal/usecase/create_transaction/create_transaction.go
+++ microsservico_wallet-core/internal/usecase/create_transaction/create_transaction.go
@@ -1,9 +1,11 @@
 package create_transaction
 
 import (
+	"context"
 	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/entity"
 	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/gateway"
 	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/pkg/events"
+	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/pkg/uow"
 )
 
 type CreateTransactionInputDTO struct {
@@ -13,53 +15,105 @@ type CreateTransactionInputDTO struct {
 }
 
 type CreateTransactionOutputDTO struct {
-	ID string
+	ID            string
+	AccountIDFrom string
+	AccountIDTo   string
+	Amount        float64
+}
+
+type BalanceUpdatedOutputDTO struct {
+	AccountIDFrom        string
+	AccountIDTo          string
+	BalanceAccountIDFrom float64
+	BalanceAccountIDTo   float64
 }
 
 type CreateTransactionUseCase struct {
-	TransactionGateway gateway.TransactionGateway
-	AccountGateway     gateway.AccountGateway
+	Uow                uow.UowInterface //Todos os repositórios serão consolidados dentro dessa "Uow".
 	EventDispatcher    events.EventDispatcherInterface
 	TransactionCreated events.EventInterface
 }
 
-func NewCreateTransactionUseCase(t gateway.TransactionGateway, a gateway.AccountGateway,
+func NewCreateTransactionUseCase(Uow uow.UowInterface,
 	eventDispatcher events.EventDispatcherInterface,
 	transactionCreated events.EventInterface) *CreateTransactionUseCase {
 	return &CreateTransactionUseCase{
-		TransactionGateway: t,
-		AccountGateway:     a,
+		Uow:                Uow,
 		EventDispatcher:    eventDispatcher,
 		TransactionCreated: transactionCreated,
 	}
 }
 
-func (uc *CreateTransactionUseCase) Execute(input *CreateTransactionInputDTO) (*CreateTransactionOutputDTO, error) {
+func (uc *CreateTransactionUseCase) Execute(ctx context.Context, input CreateTransactionInputDTO) (*CreateTransactionOutputDTO, error) {
 
-	accountFrom, err := uc.AccountGateway.FindByID(input.AccountIDFrom)
+	output := &CreateTransactionOutputDTO{}
+	//balanceUpdatedOutput := &BalanceUpdatedOutputDTO{}
+	err := uc.Uow.Do(ctx, func(_ *uow.Uow) error {
+		accountRepository := uc.getAccountRepository(ctx)
+		transactionRepository := uc.getTransactionRepository(ctx)
+
+		accountFrom, err := accountRepository.FindByID(input.AccountIDFrom)
+		if err != nil {
+			return err
+		}
+		accountTo, err := accountRepository.FindByID(input.AccountIDTo)
+		if err != nil {
+			return err
+		}
+		transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
+		if err != nil {
+			return err
+		}
+
+		err = accountRepository.UpdateBalance(accountFrom)
+		if err != nil {
+			return err
+		}
+
+		err = accountRepository.UpdateBalance(accountTo)
+		if err != nil {
+			return err
+		}
+
+		err = transactionRepository.Create(transaction)
+		if err != nil {
+			return err
+		}
+		output.ID = transaction.ID
+		output.AccountIDFrom = input.AccountIDFrom
+		output.AccountIDTo = input.AccountIDTo
+		output.Amount = input.Amount
+
+		//balanceUpdatedOutput.AccountIDFrom = input.AccountIDFrom
+		//balanceUpdatedOutput.AccountIDTo = input.AccountIDTo
+		//balanceUpdatedOutput.BalanceAccountIDFrom = accountFrom.Balance
+		//balanceUpdatedOutput.BalanceAccountIDTo = accountTo.Balance
+		return nil
+	})
 	if err != nil {
 		return nil, err
-	}
-
-	accountTo, err := uc.AccountGateway.FindByID(input.AccountIDTo)
-	if err != nil {
-		return nil, err
-	}
-
-	transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
-	err = uc.TransactionGateway.Create(transaction)
-	if err != nil {
-		return nil, err
-	}
-
-	output := &CreateTransactionOutputDTO{
-		ID: transaction.ID,
 	}
 
 	uc.TransactionCreated.SetPayload(output)
-
-	/* Agora, estamos disparando o evento após a execução desse output ocorrer. */
 	uc.EventDispatcher.Dispatch(uc.TransactionCreated)
 
+	//uc.BalanceUpdated.SetPayload(balanceUpdatedOutput)
+	//uc.EventDispatcher.Dispatch(uc.BalanceUpdated)
 	return output, nil
+}
+
+func (uc *CreateTransactionUseCase) getAccountRepository(ctx context.Context) gateway.AccountGateway {
+	repo, err := uc.Uow.GetRepository(ctx, "AccountDB")
+	if err != nil {
+		panic(err)
+	}
+	return repo.(gateway.AccountGateway)
+}
+
+func (uc *CreateTransactionUseCase) getTransactionRepository(ctx context.Context) gateway.TransactionGateway {
+	repo, err := uc.Uow.GetRepository(ctx, "TransactionDB")
+	if err != nil {
+		panic(err)
+	}
+	return repo.(gateway.TransactionGateway)
 }
--- microsservico_wallet-core/internal/usecase/create_transaction/create_transaction_test.go
+++ microsservico_wallet-core/internal/usecase/create_transaction/create_transaction_test.go
@@ -1,58 +1,27 @@
 package create_transaction
 
 import (
+	"context"
 	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/entity"
-	event2 "github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/event"
+	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/event"
+	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/internal/usecase/mocks"
 	"github.com.br/Rafiere/course_fullcycle_arquitetura-baseada-em-microsservicos/microsservico_wallet-core/pkg/events"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 )
 
-type TransactionGatewayMock struct {
-	mock.Mock
-}
-
-func (t *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
-	args := t.Called(transaction)
-	return args.Error(0)
-}
-
-/* Abaixo, temos os mocks de "account" */
-type AccountGatewayMock struct {
-	mock.Mock
-}
-
-func (a *AccountGatewayMock) Save(account *entity.Account) error {
-
-	args := a.Called(account)
-
-	return args.Error(0)
-}
-
-func (a *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
-
-	args := a.Called(id)
-
-	return args.Get(0).(*entity.Account), args.Error(1)
-}
-
 func TestCreateTransactionUseCase_Execute(t *testing.T) {
-
-	client1, _ := entity.NewClient("client1", "client1@email.com")
+	client1, _ := entity.NewClient("client1", "j@j.com")
 	account1 := entity.NewAccount(client1)
 	account1.Credit(1000)
 
-	client2, _ := entity.NewClient("client2", "client2@email.com")
+	client2, _ := entity.NewClient("client2", "j@j2.com")
 	account2 := entity.NewAccount(client2)
 	account2.Credit(1000)
 
-	mockAccount := &AccountGatewayMock{}
-	mockAccount.On("FindByID", account1.ID).Return(account1, nil)
-	mockAccount.On("FindByID", account2.ID).Return(account2, nil)
-
-	mockTransaction := &TransactionGatewayMock{}
-	mockTransaction.On("Create", mock.Anything).Return(nil)
+	mockUow := &mocks.UowMock{}
+	mockUow.On("Do", mock.Anything, mock.Anything).Return(nil)
 
 	inputDto := CreateTransactionInputDTO{
 		AccountIDFrom: account1.ID,
@@ -61,14 +30,14 @@ func TestCreateTransactionUseCase_Execute(t *testing.T) {
 	}
 
 	dispatcher := events.NewEventDispatcher()
-	event := event2.NewTransactionCreated()
-	uc := NewCreateTransactionUseCase(mockTransaction, mockAccount, dispatcher, event)
+	eventTransaction := event.NewTransactionCreated()
+	//eventBalance := event.NewBalanceUpdated()
+	ctx := context.Background()
 
-	output, err := uc.Execute(&inputDto)
+	uc := NewCreateTransactionUseCase(mockUow, dispatcher, eventTransaction)
+	output, err := uc.Execute(ctx, inputDto)
 	assert.Nil(t, err)
 	assert.NotNil(t, output)
-	mockAccount.AssertExpectations(t)
-	mockTransaction.AssertExpectations(t)
-	mockAccount.AssertNumberOfCalls(t, "FindByID", 2)
-	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+	mockUow.AssertExpectations(t)
+	mockUow.AssertNumberOfCalls(t, "Do", 1)
 }
